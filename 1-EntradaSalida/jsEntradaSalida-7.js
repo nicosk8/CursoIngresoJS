f2907ea8d6f7d0ed0a986aac60965b053fb5a074
nicosk8 <60570415+nicosk8@users.noreply.github.com>
2020-02-27 15:09:26 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,44 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var n1=document.getElementById('numeroUno').value;
+	var n2=document.getElementById('numeroDos').value;
+	n1=parseInt(n1);
+	n2=parseInt(n2);
+	var resultado=n1+n2;
+	alert('El resultado de la suma es ' + resultado);
 }
 
 function restar()
 {
+	var n1=document.getElementById('numeroUno').value;
+	var n2=document.getElementById('numeroDos').value;
+	n1=parseInt(n1);
+	n2=parseInt(n2);
+	var resultado=n1-n2;
+	alert('El resultado de la resta es ' + resultado);
 	
 }
 
 function multiplicar()
 { 
-	
+	var n1=document.getElementById('numeroUno').value;
+	var n2=document.getElementById('numeroDos').value;
+	n1=parseInt(n1);
+	n2=parseInt(n2);
+	var resultado=n1*n2;
+	alert('El resultado de la multiplicacion es ' + resultado);
 }
 
 function dividir()
 {
+	var n1=document.getElementById('numeroUno').value;
+	var n2=document.getElementById('numeroDos').value;
+	n1=parseInt(n1);
+	n2=parseInt(n2);
+	var resultado=n1/n2;
+	//var resultado=Math.round(resultado);
+	alert('El resultado de la suma es ' + resultado);
 	
 }
 
